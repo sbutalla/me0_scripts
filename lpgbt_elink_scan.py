3b0f7d4b5d29883280377b47d30e9c7db7156c59
Abhisek Datta <abhisek.datta@cern.ch>
2021-03-18 18:57:57 -0700

    correction

--- lpgbt_elink_scan.py
+++ lpgbt_elink_scan.py
@@ -62,9 +62,9 @@ def lpgbt_elink_scan(system, vfat_list):
 
     n_err_vfat_elink = {}
     for vfat in vfat_list: # Loop over all vfats
+        n_err_vfat_elink[vfat] = {}
         for elink in range(0,28): # Loop for all 28 RX elinks
             print ("VFAT%02d , ELINK %02d" % (vfat, elink))
-            n_err_vfat_elink[vfat] = {}
             # Disable RX elink under test
             setVfatRxEnable(system, vfat, 0, elink)
 
