33c21270c61fc405c48cde3d9025dd73538be0c9
Abhisek Datta <abhisek.datta@cern.ch>
2021-04-24 09:36:54 -0700

    add reg rw for fec error rate check

--- lpgbt_bert_fec.py
+++ lpgbt_bert_fec.py
@@ -2,8 +2,58 @@ from rw_reg_lpgbt import *
 from time import time
 import sys
 import argparse
+import random
 
-def check_fec_errors(system, boss, path, ohid, gbtid, runtime, verbose):
+# VFAT number: boss/sub, ohid, gbtid, elink
+# For GE2/1 GEB + Pizza
+VFAT_TO_ELINK = {
+        0  : ("sub"  , 0, 1, 6),
+        1  : ("sub"  , 0, 1, 24),
+        2  : ("sub"  , 0, 1, 11),
+        3  : ("boss" , 0, 0, 3),
+        4  : ("boss" , 0, 0, 27),
+        5  : ("boss" , 0, 0, 25),
+        6  : ("boss" , 1, 0, 6),
+        7  : ("boss" , 1, 0, 16),
+        8  : ("sub"  , 1, 1, 18),
+        9  : ("boss" , 1, 0, 15),
+        10 : ("sub"  , 1, 1, 3),
+        11 : ("sub"  , 1, 1, 17)
+}
+
+# For ME0 GEB
+#VFAT_TO_ELINK = {
+#        0  : ("boss" , 0, 0, 6),
+#        1  : ("sub"  , 0, 1, 24),
+#        2  : ("boss" , 0, 0, 11),
+#        3  : ("boss" , 0, 0, 3),
+#        4  : ("sub"  , 0, 1, 27),
+#        5  : ("sub"  , 0, 1, 25),
+#        6  : ("boss" , 1, 0, 6), # 3
+#        7  : ("sub"  , 1, 1, 24), # 25
+#        8  : ("boss" , 1, 0, 27), # 24
+#        9  : ("boss" , 1, 0, 6), # 27
+#        10 : ("sub"  , 1, 1, 27), # 6
+#        11 : ("sub"  , 1, 1, 25) # 11
+#}
+
+# Register to read/write
+vfat_registers = {
+        "HW_ID": "r",
+        "HW_ID_VER": "r",
+        "TEST_REG": "rw",
+        "HW_CHIP_ID": "r"
+}
+
+def vfat_to_oh_gbt_elink(vfat):
+    lpgbt = VFAT_TO_ELINK[vfat][0]
+    ohid  = VFAT_TO_ELINK[vfat][1]
+    gbtid = VFAT_TO_ELINK[vfat][2]
+    elink = VFAT_TO_ELINK[vfat][3]
+    return lpgbt, ohid, gbtid, elink
+
+
+def check_fec_errors(system, boss, path, ohid, gbtid, runtime, vfat_list, verbose):
 
     print ("Checking FEC Errors for: " + path)
     fec_errors = 0
@@ -14,6 +64,9 @@ def check_fec_errors(system, boss, path, ohid, gbtid, runtime, verbose):
         write_backend_reg(node, 0x001)
         
         fec_node = get_rwreg_node('GEM_AMC.OH_LINKS.OH%d.GBT%d_FEC_ERR_CNT' % (ohid, gbtid))
+        vfat_node = []
+        for vfat in vfat_list:
+            vfat_node.append(get_rwreg_node('GEM_AMC.OH.OH%d.GEB.VFAT%d.%s' % (ohid, vfat-6*gbtid, "TEST_REG")))
         
         # start error counting loop
         start_fec_errors = read_backend_reg(fec_node)
@@ -23,6 +76,15 @@ def check_fec_errors(system, boss, path, ohid, gbtid, runtime, verbose):
         time_prev = t0
         
         while ((time()-t0)/60.0) < runtime:
+            for v_node in vfat_node:
+                data_write = random.randint(0, (2**32 - 1)) # random number to write (32 bit)
+                write_backend_reg(v_node, data_write)
+                data_read = read_backend_reg(v_node)
+                if system=="backend":
+                    if data_read!=data_write:
+                        print (Colors.RED + "Register value mismatch\n" + Colors.ENDC)
+                        sys.exit()
+
             curr_fec_errors = read_backend_reg(fec_node)
             time_passed = (time()-time_prev)/60.0
             if time_passed >= 1:
@@ -85,7 +147,8 @@ if __name__ == '__main__':
     parser.add_argument("-g", "--gbtid", action="store", dest="gbtid", help="gbtid = 0, 1 (only needed for backend)") 
     parser.add_argument("-p", "--path", action="store", dest="path", help="path = uplink, downlink")
     parser.add_argument("-t", "--time", action="store", dest="time", help="TIME = measurement time in minutes")
-    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="VERBOSE")
+    parser.add_argument("-v", "--vfats", action="store", dest="vfats", nargs='+', help="vfats = list of VFATs (0-11) for read/write TEST_REG")
+    parser.add_argument("-z", "--verbose", action="store_true", dest="verbose", help="VERBOSE")
     args = parser.parse_args()
 
     if args.system == "chc":
@@ -120,12 +183,12 @@ if __name__ == '__main__':
     if boss is None:
         sys.exit()
       
-    if args.system == "backend":
+    if args.system == "backend" or args.system == "dryrun":
         if args.ohid is None:
-            print (Colors.YELLOW + "Need OHID for backend" + Colors.ENDC)
+            print (Colors.YELLOW + "Need OHID for backend/dryrun" + Colors.ENDC)
             sys.exit()
         if args.gbtid is None:
-            print (Colors.YELLOW + "Need GBTID for backend" + Colors.ENDC)
+            print (Colors.YELLOW + "Need GBTID for backend/dryrun" + Colors.ENDC)
             sys.exit()
         if int(args.ohid)>7:
             print (Colors.YELLOW + "Only OHID 0-7 allowed" + Colors.ENDC)
@@ -156,6 +219,22 @@ if __name__ == '__main__':
         print (Colors.YELLOW + "BERT measurement time required" + Colors.ENDC)
         sys.exit()
 
+    vfat_list = []
+    if args.vfats is not None:
+        if args.path != "uplink":
+            print (Colors.YELLOW + "VFAT only for uplink" + Colors.ENDC)
+            sys.exit()
+        for v in args.vfats:
+            v_int = int(v)
+            if v_int not in range(0,12):
+                print (Colors.YELLOW + "Invalid VFAT number, only allowed 0-11" + Colors.ENDC)
+                sys.exit()
+            lpgbt, oh_select, gbt_select, elink = vfat_to_oh_gbt_elink(v_int)
+            if lpgbt!=args.lpgbt or oh_select!=int(args.ohid) or gbt_select!=int(args.gbtid):
+                print (Colors.YELLOW + "Invalid VFAT number for selected lpGBT" + Colors.ENDC)
+                sys.exit()
+            vfat_list.append(v_int)
+
     # Parsing Registers XML File
     print("Parsing xml file...")
     parseXML()
@@ -177,7 +256,7 @@ if __name__ == '__main__':
             check_lpgbt_ready()
 
     try:
-        check_fec_errors(args.system, boss, args.path, int(args.ohid), int(args.gbtid), float(args.time), args.verbose)
+        check_fec_errors(args.system, boss, args.path, int(args.ohid), int(args.gbtid), float(args.time), vfat_list, args.verbose)
  
     except KeyboardInterrupt:
         print (Colors.RED + "\nKeyboard Interrupt encountered" + Colors.ENDC)
